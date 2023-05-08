7575a3985c2ee661a9bd2c0838850663d49ec584
hftamayo <hftamayo@gmail.com>
2023-05-08 10:06:15 -0600

    Student model first approach

---
+++ app/src/main/java/com/hftamayo/kotlincrudsqlite/StudentModel.kt
@@ -0,0 +1,17 @@
+package com.hftamayo.kotlincrudsqlite;
+
+import java.util.*
+
+data class StudentModel (
+    var id: Int = getAutoId(),
+    var name: String = "",
+    var email: String = ""
+
+){
+    companion object{
+        fun getAutoId():Int{
+            val random = Random()
+            return random.nextInt(100)
+        }
+    }
+}
