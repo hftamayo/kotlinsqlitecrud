350fe08995ba303504a725f7569b07dcf549e42d
hftamayo <hftamayo@gmail.com>
2023-05-08 09:47:12 -0600

    object for sqlite management

--- app/src/main/java/com/hftamayo/kotlincrudsqlite/SQLiteHelper.kt
+++ app/src/main/java/com/hftamayo/kotlincrudsqlite/SQLiteHelper.kt
@@ -1,6 +1,26 @@
 package com.hftamayo.kotlincrudsqlite
 
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 
-class SQLiteHelper(context:Context): SQLiteHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class SQLiteHelper(context:Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object{
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "student.db"
+        private const val TBL_STUDENT = "tbl_student"
+        private const val ID = "id"
+        private const val NAME = "name"
+        private const val EMAIL = "email"
+    }
+    override fun onCreate(p0: SQLiteDatabase?) {
+
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
