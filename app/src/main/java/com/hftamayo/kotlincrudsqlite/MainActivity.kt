0c31825903c66d9f0dba1fd458f4d6623d3ef4b9
hftamayo <hftamayo@gmail.com>
2023-05-09 10:10:31 -0600

    adding business logic for adding a record to the DB

--- app/src/main/java/com/hftamayo/kotlincrudsqlite/MainActivity.kt
+++ app/src/main/java/com/hftamayo/kotlincrudsqlite/MainActivity.kt
@@ -1,9 +1,36 @@
 package com.hftamayo.kotlincrudsqlite
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var edName: EditText
+    private lateinit var edEmail: EditText
+    private lateinit var btnAdd: Button
+    private lateinit var btnView: Button
+
+    private lateinit var sqLiteHelper: SQLiteHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initView()
+        sqLiteHelper = SQLiteHelper(this)
+    }
+
+    private fun initView(){
+        edName = findViewById(R.id.edName)
+        edEmail = findViewById(R.id.edEmail)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnView = findViewById(R.id.btnView)
+    }
+
+
 
 
 }
