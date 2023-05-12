f209e7d2bcede41068d43201044468fc9b87a425
hftamayo <hftamayo@gmail.com>
2023-05-12 10:18:17 -0600

    studentAdapter part2

--- app/src/main/java/com/hftamayo/kotlincrudsqlite/StudentAdapter.kt
+++ app/src/main/java/com/hftamayo/kotlincrudsqlite/StudentAdapter.kt
@@ -1,33 +1,39 @@
 package com.hftamayo.kotlincrudsqlite
 
-import android.view.TextureView
+import android.view.LayoutInflater
+import android.widget.TextView
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class StudentAdapter : RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
+    private var stdList: ArrayList<StudentModel> = ArrayList()
+
+    fun addItems(items: ArrayList<StudentModel> ){
+        this.stdList = items
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = StudentViewHolder (
+        LayoutInflater.from(parent.context).inflate(R.layout.card_items_std, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        val std = stdList[position]
+        holder.bindView(std)
+    }
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
     class StudentViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        private var id = view.findViewById<TextureView>(R.id.tvId)
-        private var name = view.findViewById<TextureView>(R.id.tvName)
-        private var email = view.findViewById<TextureView>(R.id.tvEmail)
-        private var btnDelete = view.findViewById<TextureView>(R.id.btnDelete)
+        private var id = view.findViewById<TextView>(R.id.tvId)
+        private var name = view.findViewById<TextView>(R.id.tvName)
+        private var email = view.findViewById<TextView>(R.id.tvEmail)
+        private var btnDelete = view.findViewById<TextView>(R.id.btnDelete)
         fun bindView(std:StudentModel){
             id.text = std.id.toString()
             name.text = std.name
             email.text = std.email
 
         }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
-        TODO("Not yet implemented")
     }
 }
